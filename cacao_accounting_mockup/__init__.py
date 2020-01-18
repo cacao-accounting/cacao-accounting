26885c0757899ccab478d893a834f8caf1724555
William José Moreno Reyes <williamjmorenor@gmail.com>
2020-01-18 17:22:58 -0600

    Agrega las primeras pantallas

--- cacao_accounting_mockup/__init__.py
+++ cacao_accounting_mockup/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return redirect("/login")
+
+@app.route("/login")
+def login():
+    return render_template("auth/login.html")
 
 if __name__ == "__main__":
     app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from cacao_accounting_mockup import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
