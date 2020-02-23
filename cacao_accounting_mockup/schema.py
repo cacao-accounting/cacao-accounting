ce292b1eaea9aea521329ecf2307c1dd6f2112bc
William José Moreno Reyes <williamjmorenor@gmail.com>
2020-02-23 12:30:30 -0600

    Tabla inicial de usuarios
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting_mockup/schema.py
+++ cacao_accounting_mockup/schema.py
@@ -1,1 +1,9 @@
 from cacao_accounting_mockup import db
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(20), unique=True, nullable=False)
+    nombre = db.Column(db.String(80), nullable=False)
+    apellido = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
