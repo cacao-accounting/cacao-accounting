f8617465644b4e9b5f714211110d3a76960cc7fe
William José Moreno Reyes <williamjmorenor@gmail.com>
2020-02-23 12:00:37 -0600

    Base de datos
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting_mockup/__init__.py
+++ cacao_accounting_mockup/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:test.db'
+db = SQLAlchemy(app)
 
 
 @app.route("/")
---
+++ cacao_accounting_mockup/schema.py
@@ -0,0 +1,1 @@
+from cacao_accounting_mockup import db
