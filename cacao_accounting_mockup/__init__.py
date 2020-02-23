3a3e58f8becae16f318c12f087194496d3e5206f
William José Moreno Reyes <williamjmorenor@gmail.com>
2020-02-23 12:30:06 -0600

    Arregla la linea de conección a sqlite
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting_mockup/__init__.py
+++ cacao_accounting_mockup/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
