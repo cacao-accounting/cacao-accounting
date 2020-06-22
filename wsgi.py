fb981387ef9775d366240dc2469384b667548e86
William Moreno Reyes <williamjmorenor@fedoraproject.org>
2020-06-21 21:32:27 -0600

    Establece SECRET_KEY to 'dev'
    Signed-off-by: William Moreno Reyes <williamjmorenor@fedoraproject.org>

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ app = create_app(config)
 if DEVELOPMENT:
     app.config["EXPLAIN_TEMPLATE_LOADING"] = True
     app.config["DEBUG"] = True
+    app.config["SECRET_KEY "] = "dev"
 
 if __name__ == "__main__":
     app.run()
