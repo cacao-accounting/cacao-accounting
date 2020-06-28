49846fcd0b9d6531ffdf710ba4de87030fbcf9b1
William Moreno Reyes <williamjmorenor@fedoraproject.org>
2020-06-28 16:28:00 -0600

    Agrega mas entidades al modelo de datos
    Signed-off-by: William Moreno Reyes <williamjmorenor@fedoraproject.org>

--- cacao_accounting/database/__init__.py
+++ cacao_accounting/database/__init__.py
@@ -27,3 +27,41 @@ class Usuario(UserMixin, db.Model):
     p_apellido = db.Column(db.String(80))
     s_apellido = db.Column(db.String(80))
     correo_e = db.Column(db.String(150), unique=True, nullable=False)
+    acceso = db.Column(db.Binary())
+
+
+class Pais(db.Model):
+    id = db.Column(db.String(5), primary_key=True)
+    moneda = db.Column(db.String(5), db.ForeignKey('moneda.id'))
+
+
+
+class Idioma(db.Model):
+    id = db.Column(db.String(5), primary_key=True)
+    nombre = db.Column(db.String(50), primary_key=True)
+
+
+class Moneda(db.Model):
+    id = db.Column(db.String(5), primary_key=True)
+    singular = db.Column(db.String(50))
+    plural = db.Column(db.String(50))
+    simbolo = db.Column(db.String(5))
+
+
+class TasaDeCambio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    moneda_base = db.Column(db.String(5), db.ForeignKey('moneda.id'))
+    moneda_destino = db.Column(db.String(5), db.ForeignKey('moneda.id'))
+    fecha = db.Column(db.Date())
+    tasa = db.Column(db.Float())
+
+
+class Entidad(db.Model):
+    id = db.Column(db.String(5), primary_key=True)
+    razon_social = db.Column(db.String(100), unique=True, nullable=False)
+    nombre_comercial = db.Column(db.String(50))
+    nit = db.Column(db.String(50), unique=True, nullable=False)
+    corre_electronico = db.Column(db.String(50))
+    nombre_comercial = db.Column(db.String(50))
+    moneda = db.Column(db.String(5), db.ForeignKey('moneda.id'))
+    pais = db.Column(db.String(5), db.ForeignKey('pais.id'))
