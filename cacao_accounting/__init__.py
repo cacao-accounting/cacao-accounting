946b51238906b75b4d503f846b0a034f01dea331
William Jose Moreno Reyes <williamjmorenor@gmail.com>
2020-07-13 20:58:28 -0600

    Correcion de estilo
    Signed-off-by: William Jose Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting/__init__.py
+++ cacao_accounting/__init__.py
@@ -31,7 +31,7 @@ from cacao_accounting.contabilidad import contabilidad
 from cacao_accounting.database import db
 from cacao_accounting.compras import compras
 from cacao_accounting.inventario import inventario
-from cacao_accounting.tools import  archivos, plantillas
+from cacao_accounting.tools import archivos, plantillas
 from cacao_accounting.ventas import ventas
 
 
