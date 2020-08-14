ebe435f2895750699d538bcefc64dd58c10ca4ea
William Moreno <williamjmorenor@gmail.com>
2020-08-13 20:28:46 -0600

    Update __main__.py

--- cacao_accounting/__main__.py
+++ cacao_accounting/__main__.py
@@ -29,7 +29,7 @@ if DEVELOPMENT:
 
 def run():
     """Ejecuta la aplicacion con Waitress como servidor WSGI"""
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, port=8080)
 
 
 if __name__ == "__main__":
