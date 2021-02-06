85724fb06b92fb1fd03288826e0cfc223747dfb5
William Moreno Reyes <williamjmorenor@gmail.com>
2021-02-05 20:16:49 -0600

    shore: Prueba coneccion a base de datos MSSQL
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- tests/nonfree-database.py
+++ tests/nonfree-database.py
@@ -1,8 +1,20 @@
 import pytest
 from unittest import TestCase
+import pyodbc
 from cacao_accounting import create_app
 from cacao_accounting.database import db
 from cacao_accounting.datos import base_data, demo_data
-from opensource-database import Entidad, CentroCosto, Unidad, Proyecto, Moneda
 
+MSSQL = "mssql+pyodbc://SA:cacao+SQLSERVER2019@localhost:1433/cacao?driver=ODBC+Driver+17+for+SQL+Server"
 
+try:
+    from sqlalchemy import create_engine
+
+    engine = create_engine(MSSQL)
+    with engine.connect() as con:
+        rs = con.execute("SELECT @@VERSION")
+        for row in rs:
+            print(row)
+    print("MS SQL Server disponible")
+except:
+    print("MS SQL Server no disponible")
