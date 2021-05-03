7ca205e5a43cd33688b435729f3035d2a97efdf7
William José Moreno Reyes <williamjmorenor@gmail.com>
2021-05-03 13:37:28 -0600

    style: Correcciones de estilo
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- tests/funcionales/test_vistas.py
+++ tests/funcionales/test_vistas.py
@@ -148,6 +148,7 @@ def test_cambiar_status_entidad(client, auth):
     response = client.get("/accounts/entities/set_inactive/cacao")
     response = client.get("/accounts/entities/set_default/cacao")
 
+
 def test_eliminar_entidad(client, auth):
     auth.login()
     response = client.get("/accounts/entities/set_inactive/cafe")
