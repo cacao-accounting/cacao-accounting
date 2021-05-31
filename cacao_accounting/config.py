a2913161431e2f83d1b9f6a8b14542d4498b38c6
William Moreno Reyes <williamjmorenor@fedoraproject.org>
2021-05-30 21:26:13 -0600

    style: Actualiza mensajes de logs
    Signed-off-by: William Moreno Reyes <williamjmorenor@fedoraproject.org>

--- cacao_accounting/config.py
+++ cacao_accounting/config.py
@@ -123,8 +123,8 @@ if not CONFIGURACION_BASADA_EN_ARCHIVO_LOCAL and CONFIGURACION_BASADA_EN_VARIABL
     configuracion["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 else:
-    log.warning("No se pudo encontrar una fuente para la configuración de la aplicacion, revise la documentacion")
-    log.warning("Utilizando configuración predeterminada para desarrollo, no apta para producción")
+    log.warning("No se encontro una fuente para la configuración, revise la documentacion")
+    log.warning("Utilizando configuración para desarrollo, no apta para uso en producción")
     configuracion = {}
     configuracion["SQLALCHEMY_DATABASE_URI"] = SQLITE
     configuracion["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
