022f5a502b87a7b4ad3ed8606d527d972f39f249
William Moreno <williamjmorenor@gmail.com>
2021-06-29 19:34:28 -0600

    Update uuid.py

--- cacao_accounting/database/uuid.py
+++ cacao_accounting/database/uuid.py
@@ -46,9 +46,9 @@ elif DB_URI and (DB_URI.startswith("mysql") or DB_URI.startswith("mariadb")):
     COLUMNA_UUID = Column(VARCHAR(length=36), primary_key=True, nullable=True, index=True)
 
 elif DB_URI and DB_URI.startswith("mssql"):
-    from sqlalchemy.dialects.mssql import VARCHAR
+    from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 
-    COLUMNA_UUID = Column(VARCHAR(length=36), primary_key=True, nullable=True, index=True)
+    COLUMNA_UUID = Column(UNIQUEIDENTIFIER(),  primary_key=True, nullable=True, index=True)
 
 else:
     from sqlalchemy.types import String
