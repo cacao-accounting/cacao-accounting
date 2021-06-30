60d5492bfde7d74bd125df748838be50d4343a3d
William Moreno <williamjmorenor@gmail.com>
2021-06-29 19:55:55 -0600

    Update uuid.py

--- cacao_accounting/database/uuid.py
+++ cacao_accounting/database/uuid.py
@@ -38,19 +38,19 @@ with current_app.app_context():
 if DB_URI and DB_URI.startswith("postgresql"):
     from sqlalchemy.dialects.postgresql import UUID
 
-    COLUMNA_UUID = Column(UUID(as_uuid=False), primary_key=True, nullable=True, index=True)
+    COLUMNA_UUID = Column(UUID(as_uuid=False), primary_key=True, nullable=False)
 
 elif DB_URI and (DB_URI.startswith("mysql") or DB_URI.startswith("mariadb")):
     from sqlalchemy.dialects.mysql import VARCHAR
 
-    COLUMNA_UUID = Column(VARCHAR(length=36), primary_key=True, nullable=True, index=True)
+    COLUMNA_UUID = Column(VARCHAR(length=36), primary_key=True, nullable=False, index=True)
 
 elif DB_URI and DB_URI.startswith("mssql"):
     from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 
-    COLUMNA_UUID = Column(UNIQUEIDENTIFIER(),  primary_key=True, nullable=True, index=True)
+    COLUMNA_UUID = Column(UNIQUEIDENTIFIER(),  primary_key=True)
 
 else:
     from sqlalchemy.types import String
 
-    COLUMNA_UUID = Column(String(36), primary_key=True, nullable=True, index=True, default=obtiene_texto_unico)
+    COLUMNA_UUID = Column(String(36), primary_key=True, nullable=False, index=True, default=obtiene_texto_unico)
