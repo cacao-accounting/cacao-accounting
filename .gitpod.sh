e542511125891633158148e86a583531e3de6cb6
William Moreno <williamjmorenor@gmail.com>
2021-07-13 23:09:59 +0000

    fix: Actualiza configuracion de gitpod
    Signed-off-by: William Moreno <williamjmorenor@gmail.com>

--- .gitpod.sh
+++ .gitpod.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 git config pull.rebase true
-/home/gitpod/.pyenv/versions/3.8.10/bin/python -m pip install --upgrade pip
-/home/gitpod/.pyenv/versions/3.8.10/bin/python -m pip install --upgrade pip
-/home/gitpod/.pyenv/versions/3.8.10/bin/python -m pip install -r development.txt
-/home/gitpod/.pyenv/versions/3.8.10/bin/python -m pip install -e .
-/home/gitpod/.pyenv/versions/3.8.10/bin/python setup.py develop
-/home/gitpod/.pyenv/versions/3.8.10/bin/python -m flask setupdb
+python -m pip install --upgrade pip
+python -m pip install --upgrade pip
+python -m pip install -r development.txt
+python -m pip install -e .
+python setup.py develop
+python -m flask setupdb
 yarn
 export CACAO_TEST=True
 export FLASK_APP=cacao_accounting
