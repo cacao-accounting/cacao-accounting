9ed7e78882bd32d48b11f8a5a951764f390b27c5
William Moreno Reyes <williamjmorenor@gmail.com>
2021-08-15 13:41:28 -0600

    fix: Corrige error de sintaxix
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting/__init__.py
+++ cacao_accounting/__init__.py
@@ -94,7 +94,6 @@ def registrar_rutas_predeterminadas(app: Union[Flask, None] = None) -> None:
         from flask import render_template
 
         @app.errorhandler(404)
-        permisos
         def error_404(error):  # pylint: disable=W0612
 
             return render_template("404.html"), 404
