9391258b0d30e361be47b61440f8051f3b046c3a
William José Moreno Reyes <williamjmorenor@gmail.com>
2024-06-08 20:42:28 -0600

    chore: Update development files
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- .gitpod.sh
+++ .gitpod.sh
@@ -2,14 +2,10 @@
 sudo apt install -y sqlite
 git config pull.rebase true
 python -m pip install --upgrade pip
-python -m pip install --upgrade pip
 python -m pip install -r development.txt
 python -m pip install -e .
-python setup.py develop
-python -m flask setupdb
-yarn
-export CACAO_TEST=True
-export FLASK_APP=cacao_accounting
-export FLASK_DEBUG=True
-export FLASK_ENV=development
+python -m flask initdb
+cd cacao_accounting/static/
+cd ..
+cd ..
 flask run -p 8080 --debug --reload
