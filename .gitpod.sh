80baa979d30c696e44dbc898fd308c18d79957cf
William Moreno Reyes <williamjmorenor@gmail.com>
2024-06-08 20:15:32 -0600

    chore: Update development file
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- .gitpod.sh
+++ .gitpod.sh
@@ -12,5 +12,4 @@ export CACAO_TEST=True
 export FLASK_APP=cacao_accounting
 export FLASK_DEBUG=True
 export FLASK_ENV=development
-pip install hupper
-hupper -m waitress --listen=*:8080 wsgi:app
+flask run -p 8080 --debug --reload
---
+++ run_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flask run -p 8080 --debug --reload
