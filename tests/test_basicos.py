64e573a46fb7dfb36bed4494de98c0f6583cc9bc
William José Moreno Reyes <williamjmorenor@gmail.com>
2024-06-14 13:35:09 -0600

    test: Update tests
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- tests/test_basicos.py
+++ tests/test_basicos.py
@@ -1,2 +1,36 @@
-def test_dummy():
-    import cacao_accounting
+import unittest
+
+
+class TestBasicos(unittest.TestCase):
+    def setUp(self):
+        from cacao_accounting import create_app
+        from cacao_accounting.config import configuracion
+
+        self.app = create_app(configuracion)
+        self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        self.app.app_context().push()
+
+    def test_importable(self):
+        """El proyecto debe poder importarse sin errores."""
+
+        assert self.app
+
+    def test_cli(self):
+        self.app.test_cli_runner()
+
+
+class TestInstanciasClase(unittest.TestCase):
+    def setUp(self):
+        from cacao_accounting import create_app
+        from cacao_accounting.config import configuracion
+
+        self.app = create_app(configuracion)
+        self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        self.app.app_context().push()
+
+    def test_flask_subclass(self):
+        from flask import Flask
+
+        self.assertIsInstance(self.app, Flask)
