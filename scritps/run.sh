c1ab5ae15bd16852300022afc30db4f84551e87e
William José Moreno Reyes <williamjmorenor@gmail.com>
2024-06-15 20:24:24 -0600

    chore: Clean up
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>

--- scritps/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export CACAO_TEST=True
-export FLASK_DEBUG=True
-export FLASK_ENV=development
-python wsgi.py
--- scritps/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m pip install -r development.txt
-python -m pip install -e .
-python -m flask setupdb
-yarn
