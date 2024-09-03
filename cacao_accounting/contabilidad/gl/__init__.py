43c1de5d40e2983b0989f6cff96f5b1ec116777a
William Moreno Reyes <williamjmorenor@gmail.com>
2024-09-03 15:37:57 -0600

    fix: Login required
    Signed-off-by: William José Moreno Reyes <williamjmorenor@gmail.com>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting/contabilidad/gl/__init__.py
+++ cacao_accounting/contabilidad/gl/__init__.py
@@ -22,20 +22,28 @@
 # Librerias de terceros
 # ---------------------------------------------------------------------------------------
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 # ---------------------------------------------------------------------------------------
 # Recursos locales
 # ---------------------------------------------------------------------------------------
+from cacao_accounting.decorators import modulo_activo, verifica_acceso
 
 gl = Blueprint("gl", __name__, static_folder="static", template_folder="templates")
 
 
+@login_required
+@modulo_activo("accounting")
+@verifica_acceso("accounting")
 @gl.route("/list")
 def gl_list():
     """Lista de Comprobantes Contables."""
     return render_template("gl_lista.html")
 
 
+@login_required
+@modulo_activo("accounting")
+@verifica_acceso("accounting")
 @gl.route("/new")
 def gl_new():
     """Lista de Comprobantes Contables."""
