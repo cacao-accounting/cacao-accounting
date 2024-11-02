c0c1d6daeadb4e875b82f9b938416742127a1a60
William Moreno Reyes <williamjmorenor@gmail.com>
2024-11-02 16:11:45 -0600

    docs: Update docs
    Signed-off-by: William Moreno Reyes CP|MBA <wmoreno@bmogroup.solutions>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

---
+++ docs/oci_files/mysql.sh
@@ -0,0 +1,24 @@
+podman pod create --name cacao-mysql -p 9980:80 -p 9443:443 -p 9443:443/udp
+
+podman volume create cacao-mysql-backup
+
+podman run --pod cacao-mysql --rm --replace --init --name cacao-mysql-db \
+    --volume cacao-mysql-backup:/var/lib/mysql  \
+    -e MYSQL_ROOT_PASSWORD=cacaodb \
+    -e MYSQL_DATABASE=cacaodb \
+    -e MYSQL_USER=cacaodb \
+    -e MYSQL_PASSWORD=cacaodb \
+    -d docker.io/library/mysql:8
+
+podman run --pod cacao-mysql --rm --replace --init --name cacao-mysql-server \
+    -v ./Caddyfile:/etc/caddy/Caddyfile:z \
+    -v caddy_data:/data \
+    -v caddy_config:/config \
+    -d docker.io/library/caddy:alpine
+
+podman run --pod cacao-mysql --rm --replace --init --name cacao-mysql-app \
+    -e CACAO_KEY=nsjksldknsdlkd532445yryrgfhdyyreysljdn \
+    -e CACAO_DB=mysql+pymysql://cacaodb:cacaodb@localhost:3306/cacaodb \
+    -e CACAO_USER=cacaouser \
+    -e CACAO_PWD=cacappwd \
+    -d quay.io/cacaoaccounting/cacaoaccounting
