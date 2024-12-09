f1a44a7460663a8007a37a9958e89789563b7e72
William Moreno Reyes <williamjmorenor@gmail.com>
2024-12-08 18:28:16 -0600

    release: v0.0.0.dev20241208
    Signed-off-by: William Moreno Reyes CP|MBA <wmoreno@bmogroup.solutions>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- docs/oci_files/psql.sh
+++ docs/oci_files/psql.sh
@@ -1,8 +1,8 @@
-podman pod create --name cacao-psql -p 9981:80 -p 9444:443 -p 9444:443/udp
+podman pod create --replace --name cacao-psql -p 7080:80 -p 9444:443 -p 9444:443/udp
 
-podman volume create cacao-postgresql-backup
+podman volume create --ignore cacao-postgresql-backup
 
-podman run --pod cacao-psql --rm --name cacao-psql-db \
+podman run --pod cacao-psql --rm --replace --init --name cacao-psql-db \
     --volume cacao-postgresql-backup:/var/lib/postgresql/data \
     -e POSTGRES_DB=cacaodb \
     -e POSTGRES_USER=cacaodb \
@@ -11,17 +11,13 @@ podman run --pod cacao-psql --rm --name cacao-psql-db \
 
 podman run --pod cacao-psql --rm --replace --init --name cacao-psql-server \
     -v ./Caddyfile:/etc/caddy/Caddyfile:z \
-    -v caddy_data:/data \
-    -v caddy_config:/config \
+    -v caddy_pg_data:/data \
+    -v caddy_pg_config:/config \
     -d docker.io/library/caddy:alpine
 
-podman run --pod cacao-psql --rm --replace --init --name cacao-psql-cache \
-    -d docker.io/library/valkey:alpine
-
-podman run --pod cacao-psql --rm --init --name cacao-psql-app \
-    -e CACAO_KEY=nsjksldknsdlkLKJ,dsljasfsadggfhhhhf5325364dn \
+podman run --pod cacao-psql --rm --replace --init --name cacao-psql-app \
+    -e CACAO_KEY=nsjksldknsdlkLKJ,dsljasfsadggfhh+++++++ASDhhf5325364dn \
     -e CACAO_DB=postgresql+pg8000://cacaodb:cacaodb@localhost:5432/cacaodb \
     -e CACAO_USER=cacaouser \
-    -e CACAO_PSWD=cacaopwd \
-    -e redis:localhost:6379/1 \
+    -e CACAO_PSWD=cacaopswd \
     -d quay.io/cacaoaccounting/cacaoaccounting:main
