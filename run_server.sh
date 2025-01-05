e1be98145f48ccd2062a590aabb4abf9cc062dde
William José Moreno Reyes <williamjmorenor@gmail.com>
2025-01-04 20:57:53 -0600

    build: Update test scritp
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CACAO_TEST=True cacaoctl cleandb
-CACAO_TEST=True cacaoctl setupdb
+CACAO_TEST=True CACAO_USER=test CACAO_PSWD=test cacaoctl setupdb
 CACAO_TEST=True SECRET_KEY=ASD123kljaAddS flask run -p 8080 --debug --reload
