f244d96f2343f2ca893ebcc449eadea70dc2bd32
William José Moreno Reyes <williamjmorenor@gmail.com>
2025-03-26 18:58:38 -0600

    build: Update run script
    Signed-off-by: William Moreno Reyes CP|MBA <wmoreno@bmogroup.solutions>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- run.py
+++ run.py
@@ -24,13 +24,14 @@ from os import environ
 # ---------------------------------------------------------------------------------------
 from loguru import logger
 from waitress import serve
+from sqlalchemy.exc import OperationalError
 
 # ---------------------------------------------------------------------------------------
 # Recursos locales
 # ---------------------------------------------------------------------------------------
 from cacao_accounting import create_app
 from cacao_accounting.config import PORT, THREADS, configuracion
-from cacao_accounting.database import database, User
+from cacao_accounting.database.helpers import verifica_coneccion_db, usuarios_creados
 from cacao_accounting.database.helpers import inicia_base_de_datos
 
 
@@ -38,28 +39,22 @@ app = create_app(configuracion)
 user = environ.get("CACAO_USER") or "cacao"
 passwd = environ.get("CACAO_PSWD") or "cacao"
 
-with app.app_context():
+if verifica_coneccion_db(app=app):
+    with app.app_context():
 
-    try:
-        q = database.session.execute(database.select(User)).first()
-        if q:
-            check = True
-            db = True
+        if not usuarios_creados():
+            try:
+                inicia_base_de_datos(app=app, user=user, passwd=passwd, with_examples=False)
+                db = True
+            except OperationalError:
+                logger.warning("Hubo un error al inicializar la base de datos.")
+                db = False
         else:
-            check = False
-    except:
-        check = False
-
-    if not check:
-        try:
-            inicia_base_de_datos(app=app, user=user, passwd=passwd, with_examples=False)
             db = True
-        except:
-            logger.warning("Hubo un error al inicializar la base de datos.")
-            db = False
 
-    if db:
-        logger.info("Iniciando servidor WSGI en puerto {puerto}.", puerto=PORT)
-        serve(app, port=int(PORT), threads=int(THREADS))
-    else:
-        logger.warning("No se pudo iniciar el servicio.")
+
+if not db:
+    logger.warning("No se pudo iniciar el servicio.")
+else:
+    logger.info("Iniciando servidor WSGI en puerto {puerto}.", puerto=PORT)
+    serve(app, port=int(PORT), threads=int(THREADS))
