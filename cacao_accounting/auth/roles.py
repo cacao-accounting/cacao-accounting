92e92b0479a430506148adf6d32ff2b411ea6612
William Moreno Reyes <williamjmorenor@gmail.com>
2026-05-02 16:18:25 +0000

    chore: Update SPDX-License-Identifier
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting/auth/roles.py
+++ cacao_accounting/auth/roles.py
@@ -1,16 +1,5 @@
-# Copyright 2020 William José Moreno Reyes
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2025 - 2026 William José Moreno Reyes
 
 """Definicion de Roles para regular el acceso al sistema de los usuarios."""
 
@@ -186,9 +175,6 @@ def obtener_roles_por_usuario(usuario: str):
         return []
 
     ROLES_DE_USUARIO = (
-        database.session.query(RolesUser, Roles, User)
-        .join(Roles)
-        .join(User)
-        .filter(RolesUser.user_id == USUARIO.id)
+        database.session.query(RolesUser, Roles, User).join(Roles).join(User).filter(RolesUser.user_id == USUARIO.id)
     )
     return ROLES_DE_USUARIO
