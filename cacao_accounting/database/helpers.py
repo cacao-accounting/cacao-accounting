5a68a8ef45c2880c15cde63afb41d8f6d4cc370e
William Moreno <wmoreno@bmogroup.solutions>
2026-05-02 16:18:25 +0000

    chore: Update SPDX-License-Identifier
    Signed-off-by: William Moreno <wmoreno@bmogroup.solutions>

--- cacao_accounting/database/helpers.py
+++ cacao_accounting/database/helpers.py
@@ -1,16 +1,5 @@
-# Copyright 2020 William José Moreno Reyes
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2025 - 2026 William José Moreno Reyes
 """Funciones auxiliares relacionadas a la base de datos."""
 
 # ---------------------------------------------------------------------------------------
@@ -168,9 +157,7 @@ def obtener_id_modulo_por_nombre(modulo: Union[str, None]) -> Union[str, None]:
     if modulo:
         from cacao_accounting.database import Modules, database
 
-        MODULO = database.session.execute(
-            database.select(Modules).filter_by(module=modulo)
-        ).scalar_one_or_none()
+        MODULO = database.session.execute(database.select(Modules).filter_by(module=modulo)).scalar_one_or_none()
         return MODULO.id if MODULO is not None else None
     else:
         return None
