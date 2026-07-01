7e70c6178ddc63852a10c3bff68004cfbb895b99
William José Moreno Reyes <williamjmorenor@gmail.com>
2026-07-01 23:39:13 +0000

    fix(inventario): add all missing fields to FormularioArticulo
    Add brand, model_name, has_batch, has_serial_no, purchase_uom, sale_uom,
    standard_rate, last_purchase_rate, default_supplier_id, default_warehouse_id,
    min_stock_qty, max_stock_qty, reorder_level
    
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- cacao_accounting/inventario/forms.py
+++ cacao_accounting/inventario/forms.py
@@ -4,7 +4,7 @@
 """Formularios web del modulo de inventario."""
 
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, SelectField, StringField, TextAreaField
+from wtforms import BooleanField, DecimalField, SelectField, StringField, TextAreaField
 from wtforms.validators import DataRequired, Optional
 
 CODIGO = "Código"
@@ -31,6 +31,19 @@ class FormularioArticulo(FlaskForm):
     currency = SelectField("Moneda", choices=[], validators=[Optional()])
     default_uom = SelectField("UOM Base", choices=[], validators=[DataRequired()])
     barcode = StringField("Código de barras", validators=[Optional()])
+    brand = StringField("Marca", validators=[Optional()])
+    model_name = StringField("Modelo", validators=[Optional()])
+    has_batch = BooleanField("Controlar lote")
+    has_serial_no = BooleanField("Controlar número de serie")
+    purchase_uom = SelectField("UOM de compra", choices=[], validators=[Optional()])
+    sale_uom = SelectField("UOM de venta", choices=[], validators=[Optional()])
+    standard_rate = DecimalField("Tarifa estándar", places=4, validators=[Optional()])
+    last_purchase_rate = DecimalField("Última tarifa de compra", places=4, validators=[Optional()])
+    default_supplier_id = StringField("Proveedor predeterminado", validators=[Optional()])
+    default_warehouse_id = StringField("Bodega predeterminada", validators=[Optional()])
+    min_stock_qty = DecimalField("Stock mínimo", places=4, validators=[Optional()])
+    max_stock_qty = DecimalField("Stock máximo", places=4, validators=[Optional()])
+    reorder_level = DecimalField("Punto de reorden", places=4, validators=[Optional()])
 
 
 class FormularioUOM(FlaskForm):
