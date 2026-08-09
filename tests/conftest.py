7a16d4f3ef9a38b287b648cdaa26fb998a535bdd
William Jose Moreno Reyes <williamjmorenor@gmail.com>
2026-08-09 03:48:59 +0000

    test(cycles): ampliar escenarios contables full
    Agrega cobros parciales, exceso, anticipos y compensacion 3-way multimoneda con reportes de recepcion.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,8 @@
 def pytest_addoption(parser):
     parser.addoption("--slow", action="store", default="False", help="Run all tests, even those that take the longest.")
+    parser.addoption(
+        "--full",
+        action="store_true",
+        default=False,
+        help="Run and report full end-to-end business-cycle tests.",
+    )
